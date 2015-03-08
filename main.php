18d1937285d5d47c23338d351a9fc0d1c4da81b1
Meruyert Aitbayeva (Mika) <aitbayev@usc.edu>
2015-03-07 18:14:26 -0800

    data validation added to hazards_edit
    hazard edit form data validation is working

--- main.php
+++ main.php
@@ -4,22 +4,6 @@
 <?php require_once("./includes/validation_functions.php"); ?>
 <?php 
 	$style=array('listview');
-	$sort_by="priority";
-	$order="DESC";
-	if(isset($_POST["sort_by"])){
-		$sort_by=$_POST["sort_by"];
-		$_SESSION["sort_by"]=$sort_by;
-		}
-	else if(isset($_SESSION["sort_by"])){
-		$sort_by=$_SESSION["sort_by"];
-	}
-	if(isset($_POST["order"])){
-		$order=$_POST["order"];
-		$_SESSION["order"]=$order;
-		}
-	else if(isset($_SESSION["order"])){
-		$order=$_SESSION["order"];
-	}
 ?>
 
 
@@ -30,44 +14,21 @@
     
     <h2>Hazards</h2>
 	
-	<form method="post" action="" style="float:left">
-		<label>Sort By:</label>
-		<select name="sort_by" onchange="this.form.submit()">
-			<option value=<?php echo $sort_by;?>><?php echo $sort_by;?></option>
-			<option value="priority">Priority</option>
-			<option value="time">Time</option>
-		</select>
-		<noscript><input type="submit" value="Submit" name="submit"></noscript>   
-	</form>	
-	<form method="post" action="" style="float:left">
-		<select name="order" onchange="this.form.submit()">
-			<option value=<?php echo $order;?>><?php echo $order;?></option>
-			<option value="DESC">Descending</option>
-			<option value="ASC">Ascending</option>
-		</select>
-		<noscript><input type="submit" value="Submit" name="submit"></noscript>   
-	</form>	
-	<br/>
-	
 	<?php 
 		$display_limit=10;
-		$query="SELECT * FROM hazards ORDER BY {$sort_by} {$order}";
-			if($sort_by!="time")
-				$query.=", time DESC";
-			$query.=" LIMIT {$display_limit}"; 
+		$query="SELECT * FROM hazards ORDER BY priority DESC, time DESC LIMIT {$display_limit}"; 
 		$post=mysqli_query($connection,$query);
 		while($row=mysqli_fetch_assoc($post)){
-			$namequery="SELECT username FROM users WHERE id={$row["author_id"]}";
+			$namequery="SELECT name FROM users WHERE id={$row["author_id"]}";
 			$name=mysqli_fetch_assoc(mysqli_query($connection,$namequery));
 			echo "<div class=\"post\"> 
-					<a href=\"#hazard_edit.php?hazard_id={$row["id"]}\">view</a> 
-					<h2 style=\"text-align:center\">{$row["info"]} </h2> <hr/>";
-				echo "<div class='info'>";	
+					<h2 style=\"text-align:center\">{$row["info"]} </h2><hr/>";
+				echo "<div class='info'>";
 					echo "<div class='description'>{$row["info"]}</div>";
 					echo "<div class='location'>{$row["x"]}\",{$row["y"]}\"</div>";
 					echo "<div class='time'>{$row["time"]}</div>";						
 					if(!$row["anonymous"]){
-						echo "<div style='float:left'>by {$name["username"]}</div>";
+						echo "<div style='float:left'>by {$name["name"]}</div>";
 					}
 					echo "<hr class='clear'/>";
 				echo"</div> ";
