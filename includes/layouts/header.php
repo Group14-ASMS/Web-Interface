122428c0f70ebf7ae057718bd45bb7a9531cb141
imatasaf <imatasaf@usc.edu>
2015-04-16 21:35:33 -0700

    header change test

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -1,10 +1,12 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>ASMS</title>
-<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Aviation Safety Management Monitor</title>
+	<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+	<link href="stylesheets/faastyles.css" media="all" rel="stylesheet" type="text/css" />
 
 	<?php 
 		if(isset($style)){
@@ -17,8 +19,9 @@
 </head>
 <body>
     <div id="header">
-      <h1>ASMS
-	  <?php if (is_admin()) { echo ": Admin"; } ?></h1>
+      <img float="left" src="http://asms-image-storage.s3.amazonaws.com/logos/logoFAA.png" height="84" width="84"/>
+      <h1>Aviation Safety Management Monitor </h1>
+      <?php if (is_admin()) { echo ": Admin"; } ?>
 	  
 	  <?php 
 	  echo "<a href=\"./index.php\">main</a>&nbsp";
