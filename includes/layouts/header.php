7eae0a381ee328f0bfba98869779a9d3ae7662a7
imatasaf <imatasaf@usc.edu>
2015-04-16 21:40:31 -0700

    faa header

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -18,14 +18,44 @@
 	?>
 </head>
 <body>
-    <div id="header">
-      <h1>Aviation Safety Management Monitor </h1>
-      <?php if (is_admin()) { echo ": Admin"; } ?>
+    <div id="header" role="banner">
+    	<a class="visuallyHidden" href="#content">Skip to page content</a>
+		<div class="templateContainer clear">
+			<a class="siteLogo" href="index.php">
+			  <img alt="FAA seal" class="left" src="http://asms-image-storage.s3.amazonaws.com/logos/logoFAA.png" height="84" width="84">
+			  <span>Aviation Safety Management Monitor</span>
+			</a>
+	
+			<ul class="topNav" role="menubar">
+				
+						<li role="presentation">
+							<a href="http://www.faa.gov" role="menuitem">FAA Home</a>
+						</li>
+
+						<li role="presentation">
+							<a href="http://usc.edu" role="menuitem" class="here"><strong>USC Home</strong></a>
+						</li>
+					
+			</ul>
+		</div>
+
+		
+		<div id="hNav" class="hNav" role="navigation">
+
+			<ul role="menubar">
+				
+						<li role="presentation" aria-haspopup="true"><a href="/aircraft/" role="menuitem">Hazard List</a></li>
+						<li role="presentation" aria-haspopup="true"><a href="/airports/" role="menuitem">Hazard Map</a></li>
+					
+			</ul>
+		</div>
+      	<?php if (is_admin()) { echo ": Admin"; } ?>
 	  
-	  <?php 
-	  echo "<a href=\"./index.php\">main</a>&nbsp";
-	  echo " <a href=\"./map.php\">map</a>&nbsp";
-	  if(logged_in())	echo " <a href=\"./logout.php\">logout</a>&nbsp";
-	  else echo " <a href=\"./login.php\">login</a>&nbsp";?>
+	  	<?php 
+	  		echo "<a href=\"./index.php\">main</a>&nbsp";
+	  		echo " <a href=\"./map.php\">map</a>&nbsp";
+	  		if(logged_in())	echo " <a href=\"./logout.php\">logout</a>&nbsp";
+	  		else echo " <a href=\"./login.php\">login</a>&nbsp";
+	  	?>
     </div>
 
